f4292fa217dc23538adf450e55db5f51c06f7e8d
Tevin Kagwima Njau <tevin.njau@student.moringaschool.com>
2026-02-10 14:18:08 +0300

    made page to stop refreshing after the event is met

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const form = document.getElementById("place-form");
 const placesList = document.getElementById("places-list");
 
 // Listen for form submission
-form.addEventListener("submit", function (event) 
+form.addEventListener("submit", function (event) {
+    // Stop page from refreshing
+    event.preventDefault();
