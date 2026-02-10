1fea7ada9792ec1c2f446c4af0fa57c10e51838b
Tevin Kagwima Njau <tevin.njau@student.moringaschool.com>
2026-02-10 14:17:30 +0300

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 // Get important elements from the page
 const form = document.getElementById("place-form");
 const placesList = document.getElementById("places-list");
+
+// Listen for form submission
+form.addEventListener("submit", function (event) 
